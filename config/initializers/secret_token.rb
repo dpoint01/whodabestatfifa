bfd00befaf42153bc8555541041788921c8420fc
dpoint01 <david.pointeau@tufts.edu>
2014-07-07 12:49:31 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fifa::Application.config.secret_key_base = '78d95b3232cd9c9f476198ddd3111980f8020b9071a40cb639090f61abf47dbc870200292bd139a320b63565d1b32609d1d03b0081d2d5227dd1493e8a203a19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fifa::Application.config.session_store :cookie_store, key: '_fifa_session'
