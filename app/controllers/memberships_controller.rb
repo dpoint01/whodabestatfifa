7e371ea1f346e8f684a0e07e455f63c54d5da45a
dpoint01 <david.pointeau@tufts.edu>
2014-07-09 17:20:35 -0400

    still having some issues with joining validations

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,13 +1,8 @@
 class MembershipsController < ApplicationController
   def create
-    @joined = false
-    membership = Membership.create(group_id: params[:group_id], user: current_user)
 
-    if membership.save
-      @joined = true
-      redirect_to group_path(params[:group_id])
-    else
-      render :new, notice: "Your group couldnt be saved."
-    end
+    membership = Membership.create(group_id: params[:group_id], user: current_user)
+    redirect_to group_path(params[:group_id])
+
   end
 end
